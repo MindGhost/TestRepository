cf5c72a9b5b5bfc5e8f43df0b6d8b6d0532ab508
MindGhost <themindghost@gmail.com>
2013-12-25 03:58:28 +0200

    Change file

--- TestProject/TestApp.java
+++ TestProject/TestApp.java
@@ -7,11 +7,17 @@ public class TestApp
 		TestClass inst2 = new TestClass();
 		TestClass inst3 = new TestClass();
 		
+		TestClass inst4 = new TestClass();
+		
 		System.out.println("Instance 1 number: " + inst1.getInstanceNumber());
 		System.out.println("Instance 2 number: " + inst2.getInstanceNumber());
 		System.out.println("Instance 3 number: " + inst3.getInstanceNumber());
+		
 		System.out.println("Instance 1 count: " + inst1.getInstanceCount());
 		System.out.println("Instance 2 count: " + inst2.getInstanceCount());
-		System.out.println("Instance 2 count: " + inst3.getInstanceCount());
+		System.out.println("Instance 3 count: " + inst3.getInstanceCount());
+		
+		System.out.println("Instance 4 number: " + inst4.getInstanceNumber());
+		System.out.println("Instance 4 count: " + inst4.getInstanceCount());
 	}
 }
