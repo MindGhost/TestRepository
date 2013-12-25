4e57286ace1292f48ec8456030baa748212499de
MindGhost <themindghost@gmail.com>
2013-12-25 04:27:54 +0200

    Fix branch change

--- TestProject/TestClass.java
+++ TestProject/TestClass.java
@@ -4,10 +4,14 @@ public class TestClass
 	private static int mInstanceCtr = 0;
 	private int mInstanceNumber;
 	
+	private String mInstanceName;
+	
 	public TestClass()
 	{
 		mInstanceNumber = mInstanceCtr;
 		mInstanceCtr++;
+		
+		mInstanceName = "TestClass_Instance_" + mInstanceNumber;
 	}
 	
 	public int getInstanceNumber()
@@ -18,5 +22,10 @@ public class TestClass
 	public int getInstanceCount()
 	{
 		return mInstanceCtr;
+	}
+	
+	public String getInstanceName()
+	{
+		return mInstanceName;
 	}
 }
